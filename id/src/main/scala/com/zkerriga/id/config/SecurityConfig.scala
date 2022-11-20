b1285b00ca481b2dd42be2eb783dd4f4d739210e
Daniil Eletskii <deletskii@evolution.com>
2022-11-21 01:19:53 +0400

    add SecurityConfig layer

--- id/src/main/scala/com/zkerriga/id/config/SecurityConfig.scala
+++ id/src/main/scala/com/zkerriga/id/config/SecurityConfig.scala
@@ -1,7 +1,12 @@
 package com.zkerriga.id.config
 
 import com.zkerriga.id.internal.domain.password.Salt
+import zio.{URLayer, ZLayer}
 
 case class SecurityConfig(
   salt: Salt
 )
+
+object SecurityConfig:
+  val live: URLayer[AppConfig, SecurityConfig] =
+    ZLayer.fromFunction((config: AppConfig) => config.security)
