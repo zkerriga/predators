d3a0b67360335086f6555e1d064d31e7562691d0
Daniil Eletskii <deletskii@evolution.com>
2023-01-02 17:58:09 +0400

    improve login check performance

--- id/src/main/scala/com/zkerriga/id/storages/players/InMemoryUserRepo.scala
+++ id/src/main/scala/com/zkerriga/id/storages/players/InMemoryUserRepo.scala
@@ -2,32 +2,41 @@ package com.zkerriga.id.storages.players
 
 import cats.syntax.either.*
 import com.zkerriga.id.domain.player.{Login, PlayerId}
+import com.zkerriga.id.storages.players.InMemoryUserRepo.Table
 import com.zkerriga.id.storages.players.errors.LoginConflictError
 import zio.*
 
-private[players] class InMemoryUserRepo(private val tableRef: Ref[Map[PlayerId, Player]])
-    extends PlayersRepo:
+private[players] class InMemoryUserRepo private (private val ref: Ref[Table]) extends PlayersRepo:
 
   def register(player: Player): IO[LoginConflictError, Unit] =
     for
       /* todo: ZIO.fail(...) will be calculated every time because of the eager default parameter.
-       *   Will probably change it after discussion here: https://github.com/zio/zio/pull/364
+       *   Will probably switch to it after discussion here: https://github.com/zio/zio/pull/364
+       * ensureNoLoginConflict <- ref.modifySome(ZIO.fail(LoginConflictError(player.login))) {
+       *   case Table(table, logins) if !logins(player.login) =>
+       *     ZIO.unit -> Table(table.updated(player.id, player), logins + player.login)
+       * }
        */
-      ensureNoLoginConflict <- tableRef.modifySome(ZIO.fail(LoginConflictError(player.login))) {
-        case table if table.forall { case (_, p) => p.login != player.login } =>
-          ZIO.unit -> table.updated(player.id, player)
+      ensureNoLoginConflict <- ref.modify { case current @ Table(table, logins) =>
+        if logins(player.login) then ZIO.fail(LoginConflictError(player.login)) -> current
+        else ZIO.unit -> Table(table.updated(player.id, player), logins + player.login)
       }
       _ <- ensureNoLoginConflict
       _ <- debugStateOfTable
     yield ()
 
   private def debugStateOfTable =
-    tableRef.get.flatMap { table =>
+    ref.get.flatMap { table =>
       ZIO.logDebug(s"state of Players-Table is $table")
     }
 
 object InMemoryUserRepo:
+  private case class Table(table: Map[PlayerId, Player], logins: Set[Login])
+  private object Table {
+    val Empty: Table = Table(Map.empty, Set.empty)
+  }
+
   val live: ULayer[PlayersRepo] =
     ZLayer.fromZIO {
-      Ref.make(Map.empty[PlayerId, Player]).map(InMemoryUserRepo(_))
+      Ref.make(Table.Empty).map(InMemoryUserRepo(_))
     }
